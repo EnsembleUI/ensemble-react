2bcd4afca5a3140191ccaffd2de223a860df9409
sanaf1333 <sanaf1333@gmail.com>
2023-09-14 18:48:27 +0500

    removed unused file and code

--- packages/runtime/src/widgets/index.ts
+++ packages/runtime/src/widgets/index.ts
@@ -3,7 +3,6 @@ export * from "./Button";
 export * from "./Column";
 export * from "./Text";
 export * from "./Row";
-export * from "./DataGrid";
 export * from "./SideBar";
 export interface EnsembleWidgetProps {
   id?: string;
