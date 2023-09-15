364ca6855cd3f2da7f1267a25d84d88d087b3fe2
Evan Shi <4132143+evshi@users.noreply.github.com>
2023-09-15 15:00:38 -0700

    Add support for multiple screens

---
+++ packages/runtime/src/runtime/navigate.ts
@@ -0,0 +1,10 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const useNavigateScreen = (name: string): (() => void) => {
+  const navigate = useNavigate();
+  const callback = useCallback(() => {
+    navigate(name.toLowerCase());
+  }, [name, navigate]);
+  return callback;
+};
