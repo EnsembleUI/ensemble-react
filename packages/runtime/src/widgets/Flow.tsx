5e0f0849e0427b0bec66ea91c93079a252a926c5
Rahul Tibrewal <tibrewalrahul01@gmail.com>
2023-09-20 19:27:14 +0530

    item template for flow added

--- packages/runtime/src/widgets/Flow.tsx
+++ packages/runtime/src/widgets/Flow.tsx
@@ -3,15 +3,43 @@ import { WidgetRegistry } from "../registry";
 import type { EnsembleWidgetProps, FlexboxProps } from "../util/types";
 import { EnsembleRuntime } from "../runtime";
 import { Space } from "antd";
+import { CustomScope, CustomScopeProvider, Expression, Widget, useEnsembleStore } from "framework";
+import { get, map } from "lodash-es";
 
-export const Flow: React.FC<FlexboxProps> = (props) => {
+interface FlowProps {
+  "item-template": {
+    data: Expression<object>;
+    name: string;
+    template: FlowComponentTemplate;
+  };
+}
 
-  const renderedChildren = useMemo(() => {
-    return EnsembleRuntime.render(props.children);
-  }, [props.children]);
+interface FlowComponentTemplate extends Widget {
+  name: 'FlowComponents';
+  properties: {
+    children: Widget[];
+  };
+}
+
+export const Flow: React.FC<FlowProps> = ({
+  "item-template": itemTemplate,
+}) => {
+  const { templateData } = useEnsembleStore((state) => ({
+    templateData: get(state.screen, itemTemplate.data as string) as object,
+  }));
+
+  const namedData = map(templateData, (value) => {
+    const namedObj: Record<string, unknown> = {};
+    namedObj[itemTemplate.name] = value;
+    return namedObj;
+  });
 
   return (<Space wrap>
-    {renderedChildren}
+    {itemTemplate.template.properties.children.map((item, i) => {
+      return (<CustomScopeProvider value={namedData[i] as CustomScope}>
+        {EnsembleRuntime.render([item])}
+      </CustomScopeProvider>)
+    })}
   </Space>)
 };
 
