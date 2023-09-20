6e07fc4add779a840f0c8ff08163bdf3d5748d04
Rahul Tibrewal <tibrewalrahul01@gmail.com>
2023-09-20 23:01:20 +0530

    flow component changed

--- packages/runtime/src/widgets/Flow.tsx
+++ packages/runtime/src/widgets/Flow.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { WidgetRegistry } from "../registry";
-import type { EnsembleWidgetProps, FlexboxProps } from "../util/types";
 import { EnsembleRuntime } from "../runtime";
 import { Space } from "antd";
 import { CustomScope, CustomScopeProvider, Expression, Widget, useEnsembleStore } from "framework";
@@ -8,38 +7,41 @@ import { get, map } from "lodash-es";
 
 interface FlowProps {
   "item-template": {
-    data: Expression<object>;
+    data: Expression<object> | string[];
     name: string;
-    template: FlowComponentTemplate;
-  };
-}
-
-interface FlowComponentTemplate extends Widget {
-  name: 'FlowComponents';
-  properties: {
-    children: Widget[];
+    template: Widget;
   };
 }
 
 export const Flow: React.FC<FlowProps> = ({
   "item-template": itemTemplate,
 }) => {
+  let namedData: Record<string, unknown>[] | number[] | string[] = [];
+
   const { templateData } = useEnsembleStore((state) => ({
     templateData: get(state.screen, itemTemplate.data as string) as object,
   }));
 
-  const namedData = map(templateData, (value) => {
-    const namedObj: Record<string, unknown> = {};
-    namedObj[itemTemplate.name] = value;
-    return namedObj;
-  });
+  if (Array.isArray(itemTemplate.data)) {
+    namedData = map(itemTemplate.data, (value) => {
+      const namedObj: Record<string, unknown> = {};
+      namedObj[itemTemplate.name] = value;
+      return namedObj;
+    });
+  } else {
+    namedData = map(templateData, (value) => {
+      const namedObj: Record<string, unknown> = {};
+      namedObj[itemTemplate.name] = value;
+      return namedObj;
+    });
+  }
 
   return (<Space wrap>
-    {itemTemplate.template.properties.children.map((item, i) => {
-      return (<CustomScopeProvider value={namedData[i] as CustomScope}>
-        {EnsembleRuntime.render([item])}
-      </CustomScopeProvider>)
-    })}
+    {namedData.map(n => (
+      <CustomScopeProvider value={n as CustomScope}>
+        {EnsembleRuntime.render([itemTemplate.template])}
+      </CustomScopeProvider>
+    ))}
   </Space>)
 };
 
