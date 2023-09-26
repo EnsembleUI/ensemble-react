bfc862cfdd8110226c89a7e5db20b1baeeebc2ab
sanaf1333 <sanaf1333@gmail.com>
2023-09-26 21:16:34 +0500

    updated avatar group widget

--- packages/runtime/src/widgets/AvatarGroup.tsx
+++ packages/runtime/src/widgets/AvatarGroup.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useMemo } from "react";
 import type { Expression, Widget } from "framework";
+import { useEnsembleStore } from "framework";
 import { useEnsembleState, useEvaluate } from "framework";
 import { useNavigateScreen } from "../runtime/navigate";
 import { WidgetRegistry } from "../registry";
@@ -8,39 +9,67 @@ import * as MuiIcons from "@mui/icons-material";
 import { EnsembleRuntime } from "../runtime";
 import { AvatarMenu } from "./Avatar";
 import { AvatarProps } from "./Avatar";
+import { get, map } from "lodash-es";
+import { CustomScopeProvider } from "framework";
+import type { CustomScope } from "framework";
+export type AvatarItem = {
+  name: "AvatarIcon";
+  properties: {
+    alt: Expression<string>;
+    src?: Expression<string>;
+    name?: Expression<string>;
+  };
+};
 
 export type AvatarGroupProps = {
   max: number;
   "item-template": {
     data: Expression<object>;
-    name: string;
+    name: Expression<string>;
     template: AvatarTemplate;
   };
 };
 export interface AvatarTemplate {
   name: "Avatars";
   properties: {
-    children: AvatarProps[];
+    children: AvatarItem[];
   };
 }
 export const AvatarGroup: React.FC<AvatarGroupProps> = ({
   max,
   "item-template": itemTemplate,
 }) => {
-  const renderedChildren = useMemo(() => {
-    return EnsembleRuntime.render(
-      itemTemplate.template.properties.children as unknown as Widget[]
-    );
-  }, [itemTemplate.template.properties.children]);
+  //console.log(itemTemplate);
+
+  const { templateData } = useEnsembleStore((state) => ({
+    templateData: get(state.screen, itemTemplate.data as string) as object,
+  }));
+
+  const namedData = map(templateData, (value) => {
+    const namedObj: Record<string, unknown> = {};
+    namedObj[itemTemplate.name] = value;
+    return namedObj;
+  });
+  //console.log(namedData);
+
+  const [source, setSource] = useState(
+    itemTemplate.template.properties.children[0].properties
+  );
+  const { values } = useEnsembleState({ ...source, source }, undefined, {
+    setSource,
+  });
+  console.log(values);
 
   return (
     <div>
       <MuiAvatarGroup max={max}>
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-        <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        {namedData.map((item, index) => (
+          <CustomScopeProvider value={namedData as unknown as CustomScope}>
+            <Avatar key={index}>
+              {itemTemplate.template.properties.children[0].properties.name}
+            </Avatar>
+          </CustomScopeProvider>
+        ))}
       </MuiAvatarGroup>
     </div>
   );
