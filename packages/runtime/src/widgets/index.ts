2f3d72aba834d5b1b208b2c9e571b52c82a2154c
sanaf1333 <sanaf1333@gmail.com>
2023-10-02 10:57:41 +0500

    updated merge conflicts

--- packages/runtime/src/widgets/index.ts
+++ packages/runtime/src/widgets/index.ts
@@ -9,6 +9,6 @@ export * from "./Icon";
 export * from "./Row";
 export * from "./Charts";
 export * from "./DataGrid";
+export * from "./TabBar";
 export * from "./Progress";
 export * from "./GridView";
-export * from "./TabBar";
