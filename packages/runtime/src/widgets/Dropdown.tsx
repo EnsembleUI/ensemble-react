1d83de915b9f6e27beb85d05b6d0a7a9d4c4135f
sanaf1333 <sanaf1333@gmail.com>
2023-10-05 00:59:29 +0500

    updated dropdown styles

--- packages/runtime/src/widgets/Dropdown.tsx
+++ packages/runtime/src/widgets/Dropdown.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import type { Expression } from "framework";
 import { useRegisterBindings, useExecuteCode } from "framework";
 import { useNavigateScreen } from "../runtime/navigate";
 import { WidgetRegistry } from "../registry";
-import { MenuItem, Select, InputLabel, FormControl, Box } from "@mui/material";
+import { MenuItem, Select, FormControl } from "@mui/material";
 import { SelectChangeEvent } from "@mui/material/Select";
 
 export type DropdownItem = {
@@ -18,7 +18,7 @@ export type DropdownProps = {
   hintText?: Expression<string>;
   required?: boolean;
   enabled?: boolean;
-  variant?: "standard" | "filled";
+  autoComplete: boolean;
   onChange?: {
     executeCode?: string;
     navigateScreen?: string;
@@ -26,6 +26,16 @@ export type DropdownProps = {
   styles?: {
     minWidth?: number | string;
     maxWidth?: number | string;
+    variant?: "standard" | "filled" | "outlined";
+    contentPadding: string | number;
+    fillColor: string;
+    borderRadius: number;
+    borderWidth: number;
+    borderColor: string;
+    disabledBorderColor: string;
+    errorBorderColor: string;
+    focusedBorderColor: string;
+    focusedErrorBorderColor: string;
   };
   defaultValue?: string;
   menu?: (string | DropdownItem)[];
@@ -40,7 +50,7 @@ export const Dropdown: React.FC<DropdownProps> = (props) => {
       setDropdownValue,
     }
   );
-  //const { values } = useRegisterBindings(props);
+
   const onTapCallback = useExecuteCode(
     props.onChange?.executeCode || "",
     values
@@ -54,15 +64,29 @@ export const Dropdown: React.FC<DropdownProps> = (props) => {
   };
 
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }}>
+    <FormControl
+      sx={{
+        m: 1,
+        minWidth: props.styles?.minWidth ?? 120,
+        maxWidth: props.styles?.maxWidth ?? 120,
+      }}
+    >
       <Select
         value={dropdownValue}
         id={props.id}
         onChange={handleChange}
-        required={props.required}
+        required={props.required ?? true}
+        variant={props.styles?.variant ?? "outlined"}
+        style={{
+          borderRadius: props.styles?.borderRadius,
+          borderWidth: `${props.styles?.borderWidth} !important`,
+          borderColor: props.required
+            ? props.styles?.borderColor
+            : props.styles?.disabledBorderColor,
+        }}
       >
-        <MenuItem value={props.hintText}>
-          <em>None</em>
+        <MenuItem value={props.hintText ?? "None"} disabled>
+          <em>{props.hintText ?? "None"}</em>
         </MenuItem>
         {props.menu &&
           props.menu?.map((menuItem) => {
@@ -71,7 +95,11 @@ export const Dropdown: React.FC<DropdownProps> = (props) => {
             const itemLabel =
               typeof menuItem === "string" ? menuItem : menuItem.label;
             return (
-              <MenuItem key={itemValue} value={itemValue}>
+              <MenuItem
+                key={itemValue}
+                value={itemValue}
+                style={{ padding: props.styles?.contentPadding ?? 0 }}
+              >
                 {itemLabel}
               </MenuItem>
             );
