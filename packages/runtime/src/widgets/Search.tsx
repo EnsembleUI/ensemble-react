423d0b1c11a47f751ddd8fe53bd7c3c0006d4ab4
anserwaseem <hafiz.anser.waseem@gmail.com>
2023-10-04 16:38:47 +0500

    Implement Search widget

--- packages/runtime/src/widgets/Search.tsx
+++ packages/runtime/src/widgets/Search.tsx
@@ -1,49 +1,84 @@
 import React, { useState } from "react";
-import { useEnsembleStore, type Expression } from "framework";
-import { AutoComplete, Input, SelectProps } from 'antd';
+import { useTemplateData, type Expression } from "framework";
+import { AutoComplete, Input, SelectProps } from "antd";
 import { WidgetRegistry } from "../registry";
-import type { EnsembleWidgetProps } from "../util/types";
+import type { SearchStyles } from "../util/types";
 import { SearchOutlined } from "@mui/icons-material";
-import { get } from "lodash-es";
+import { get, isString } from "lodash-es";
+import { getColor, handleCurlyBraces } from "../util/utils";
+import "./Widgets.css";
+
+interface EnsembleWidgetProps<T> {
+  id?: string;
+  [key: string]: unknown;
+  styles?: T;
+}
 
 export type SearchProps = {
-  placeholder?: Expression<string>;
-  data?: unknown;
-  searchKey: Expression<string>;
-} & EnsembleWidgetProps;
+  placeholder?: string;
+  data?: Expression<object>;
+  searchKey?: string;
+} & EnsembleWidgetProps<SearchStyles>;
 
 export const Search: React.FC<SearchProps> = ({
   placeholder,
   data,
-  searchKey
+  searchKey,
+  styles,
 }) => {
-  const [options, setOptions] = useState<SelectProps<object>['options']>([]);
+  console.log("styles", styles);
+  const [options, setOptions] = useState<SelectProps<object>["options"]>([]);
 
-  const { templateData = [] } = useEnsembleStore((state) => ({
-    templateData: get(state.screen, data as string) as object,
-  }));
+  const templateData = useTemplateData(
+    isString(data) ? handleCurlyBraces(data) : (data as Expression<object>)
+  );
 
   const handleSearch = (value: string) => {
-    if(Array.isArray(templateData)) {
-      setOptions(value ? templateData.filter(t => t[searchKey].startsWith(value)).map(t => {
-        return {
-          label: t[searchKey],
-          value: t[searchKey],
-        }
-      }) : []);
+    if (Array.isArray(templateData)) {
+      setOptions(
+        value
+          ? templateData
+              .filter((item) =>
+                get(item, searchKey ?? "")
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase())
+              )
+              .map((item) => ({
+                value: get(item, searchKey ?? ""),
+                label: get(item, searchKey ?? ""),
+              }))
+          : []
+      );
     }
   };
 
   return (
     <AutoComplete
-      popupMatchSelectWidth={252}
-      style={{ width: 300 }}
+      popupMatchSelectWidth={styles?.width}
       options={options}
       onSelect={() => {}}
       onSearch={handleSearch}
       size="large"
+      allowClear
     >
-      <Input placeholder={placeholder} prefix={<SearchOutlined />} />
+      <Input
+        placeholder={placeholder}
+        prefix={<SearchOutlined />}
+        className="no-box-shadow"
+        style={{
+          width: styles?.width,
+          height: styles?.height,
+          margin: styles?.margin,
+          borderRadius: styles?.borderRadius,
+          borderWidth: styles?.borderWidth,
+          borderStyle: styles?.borderStyle,
+          borderColor: styles?.borderColor
+            ? getColor(styles?.borderColor)
+            : undefined,
+          boxShadow: "none",
+        }}
+      />
     </AutoComplete>
   );
 };
