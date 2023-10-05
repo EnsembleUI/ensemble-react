983a42164b235f1e1e3a8c20fab7b8564ed537f3
sanaf1333 <sanaf1333@gmail.com>
2023-10-06 03:26:25 +0500

    updated tag and yaml for tags

--- packages/runtime/src/widgets/Tag.tsx
+++ packages/runtime/src/widgets/Tag.tsx
@@ -17,39 +17,37 @@ export type TagProps = {
 };
 
 export const Tag: React.FC<TagProps> = (props) => {
-    
   const [text, setText] = useState(props.label);
   const { values } = useRegisterBindings({ ...props, text }, props.id, {
     setText,
   });
-console.log(values.label)
-  return (
-    <>
-      <div
-        style={{
-          display: "inline-flex",
-          justifyContent: "left",
-          alignItems: "center",
-        }}
-      >
-        <Typography.Text
-          style={{
-            backgroundColor: props.styles?.backgroundColor ?? "#e6e7e8",
-            paddingLeft: "10px",
-            paddingRight: "10px",
-            textAlign: "left",
-            borderRadius: props.styles?.borderRadius ?? 10,
-            fontWeight: "normal",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          {values.text} &nbsp;
-          {props.icon && <Icon {...props.icon} />}
-        </Typography.Text>
-      </div>
-    </>
-  );
+
+  const labels = Array.isArray(values.label)
+    ? (values.label as string[])
+    : [values.label as string];
+
+  const tagElements = labels.map((item, index) => (
+    <Typography.Text
+      key={index}
+      style={{
+        backgroundColor: props.styles?.backgroundColor ?? "#e6e7e8",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+        textAlign: "left",
+        borderRadius: props.styles?.borderRadius ?? 10,
+        fontWeight: "normal",
+        display: "inline-flex",
+        alignItems: "center",
+        margin: "5px",
+        whiteSpace: "nowrap",
+      }}
+    >
+      {item} &nbsp;
+      {props.icon && <Icon {...props.icon} />}
+    </Typography.Text>
+  ));
+
+  return <div>{tagElements}</div>;
 };
 
 WidgetRegistry.register("Tag", Tag);
