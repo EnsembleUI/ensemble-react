e084c305926fe2fe9566d2403535fd21be13c51e
Evan Shi <4132143+evshi@users.noreply.github.com>
2023-10-05 14:06:47 -0700

    Force update screen context on storage set

--- packages/framework/src/storage.ts
+++ packages/framework/src/storage.ts
@@ -4,7 +4,8 @@ export const EnsembleStorage = {
   set: (key: string, value: unknown): void => {
     const screenContext = ensembleStore.get(screenAtom);
     screenContext.storage[key] = value;
-    ensembleStore.set(screenAtom, screenContext);
+    // FIXME: do more granular updates on screen context
+    ensembleStore.set(screenAtom, { ...screenContext });
   },
   get: (key: string): unknown => {
     const screenContext = ensembleStore.get(screenAtom);
