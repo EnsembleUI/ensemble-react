2ba56ee50d2773190d3e7483488c596807dcdded
anserwaseem <hafiz.anser.waseem@gmail.com>
2023-10-05 17:44:04 +0500

    Implement Multi Select

--- packages/runtime/src/widgets/MultiSelect.tsx
+++ packages/runtime/src/widgets/MultiSelect.tsx
@@ -1,87 +1,142 @@
-import React, { useRef, useState } from 'react';
-import { Select as SelectComponent, Space } from 'antd';
-import type { InputRef } from 'antd';
-import { WidgetRegistry } from '../registry';
-import { PlusCircleOutlined } from '@ant-design/icons';
-import { Expression } from 'framework';
-import { EnsembleWidgetProps } from '../util/types';
+import React, { useEffect, useMemo, useState } from "react";
+import { Expression, useTemplateData } from "framework";
+import { WidgetRegistry } from "../registry";
+import { PlusCircleOutlined } from "@ant-design/icons";
+import { Select as SelectComponent, Space } from "antd";
+import { get } from "lodash-es";
+import { SearchStyles } from "../util/types";
+import { getColor } from "../util/utils";
+
+interface EnsembleWidgetProps<T> {
+  id?: string;
+  [key: string]: unknown;
+  styles?: T;
+}
 
 type SelectOption = {
   label: Expression<string>;
   value: Expression<string | number>;
-}
+};
+
 export type MultiSelectProps = {
+  data: Expression<SelectOption[]>;
+  labelKey?: string;
+  valueKey?: string;
+  default?: SelectOption[];
   placeholder?: Expression<string>;
-  data: SelectOption[];
-  defaultValue: string[];
-  width: string;
-  [key: string]: unknown;
-} & EnsembleWidgetProps;
+} & EnsembleWidgetProps<SearchStyles>;
 
-const MultiSelect: React.FC<MultiSelectProps> = (props) => {
-  const { placeholder, data, defaultValue, width } = props;
-  const [items, setItems] = useState(data);
-  const [name, setName] = useState('');
-  const [selectedValue, setSelectedValue] = useState<string[] | undefined>(undefined);
+const MultiSelect: React.FC<MultiSelectProps> = ({
+  data,
+  labelKey,
+  valueKey,
+  default: defaultOptions,
+  placeholder,
+  styles,
+}) => {
+  const [options, setOptions] = useState<SelectOption[]>([]);
+  const [newOption, setNewOption] = useState("");
+  const [selectedValues, setSelectedValues] = useState<string[] | undefined>(
+    defaultOptions?.map((item) => item.value?.toString())
+  );
 
-  const addItem = () => {
-    if (selectedValue) {
-      setSelectedValue([...selectedValue, name]);
-    } else {
-      setSelectedValue([name]);
+  const templateData = useMemo(() => useTemplateData(data), [data]);
+
+  useEffect(() => {
+    if (Array.isArray(templateData)) {
+      const initialOptions = templateData.map((item) => ({
+        label: get(item, labelKey ?? "label"),
+        value: get(item, valueKey ?? "value"),
+      }));
+
+      if (defaultOptions) {
+        defaultOptions.forEach((item) => {
+          if (!initialOptions.some((option) => option.value === item.value))
+            initialOptions.push(item);
+        });
+      }
+      setOptions(initialOptions);
     }
-    setItems([...items, {
-      label: name,
-      value: name
-    }])
-    setName('');
-  };
+  }, [templateData, defaultOptions]);
 
   const handleChange = (value: string[]) => {
-    setSelectedValue(value)
+    setSelectedValues(value);
+    if (newOption) {
+      setOptions([
+        ...options,
+        {
+          label: newOption,
+          value: newOption,
+        },
+      ]);
+      setNewOption("");
+    }
   };
 
   return (
     <SelectComponent
-      mode="multiple"
-      style={{ width: width || '100%' }}
-      placeholder={placeholder || 'Select'}
-      onChange={handleChange}
-      value={selectedValue}
-      defaultValue={defaultValue}
-      onSearch={(v) => {
-        if (items.some(o => o.label?.toString().startsWith(v))) {
-          setName('')
-        } else {
-          setName(v)
-        }
+      mode="tags"
+      style={{
+        width: styles?.width ?? "100%",
+        margin: styles?.margin,
+        borderRadius: styles?.borderRadius,
+        borderWidth: styles?.borderWidth,
+        borderStyle: styles?.borderStyle,
+        borderColor: styles?.borderColor
+          ? getColor(styles.borderColor)
+          : undefined,
       }}
+      placeholder={placeholder ?? "Select"}
+      onChange={handleChange}
+      value={selectedValues}
+      defaultValue={defaultOptions?.map((item) => item.value?.toString())}
       notFoundContent={<></>}
+      options={options}
+      allowClear
+      filterOption={(input, option) =>
+        option?.label
+          ?.toString()
+          .toLowerCase()
+          .startsWith(input.toLowerCase()) || false
+      }
+      onSearch={(v) => {
+        if (
+          options.some(
+            (option) =>
+              option?.label
+                ?.toString()
+                .toLowerCase()
+                .startsWith(v.toLowerCase())
+          )
+        )
+          setNewOption("");
+        else setNewOption(v);
+      }}
       dropdownRender={(menu) => (
         <>
           {menu}
-          {name && <Space
-            onClick={() => addItem()}
-            style={{
-              padding: '10px 15px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              width: '100%',
-              cursor: 'pointer'
-            }}
-          >
-            <span>There are no matches</span>
-            <Space>
-              <PlusCircleOutlined />
-              <span>{`Add "${name}"`}</span>
+          {newOption && (
+            <Space
+              style={{
+                padding: "10px 15px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                width: "100%",
+                cursor: "pointer",
+              }}
+            >
+              <span>There are no matches</span>
+              <Space>
+                <PlusCircleOutlined />
+                <span>{`Add "${newOption}"`}</span>
+              </Space>
             </Space>
-          </Space>}
+          )}
         </>
       )}
-      options={items}
     />
-  )
+  );
 };
 
 WidgetRegistry.register("MultiSelect", MultiSelect);
