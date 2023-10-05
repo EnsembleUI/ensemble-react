a24f10c282c780b6bbf3ca354989b24c31cbb76a
Yifeng Evan Shi <4132143+evshi@users.noreply.github.com>
2023-10-05 14:00:24 -0700

    Bind multi select values and options (#103)

--- packages/runtime/src/widgets/MultiSelect.tsx
+++ packages/runtime/src/widgets/MultiSelect.tsx
@@ -1,16 +1,19 @@
+import type { ReactElement } from "react";
 import React, { useEffect, useState } from "react";
-import { Expression, useTemplateData } from "framework";
-import { WidgetRegistry } from "../registry";
+import type { Expression } from "framework";
+import { useRegisterBindings, useTemplateData } from "framework";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import { Select as SelectComponent, Space } from "antd";
 import { get } from "lodash-es";
-import { SearchStyles } from "../util/types";
-import { EnsembleWidgetProps, getColor } from "../util/utils";
+import { WidgetRegistry } from "../registry";
+import type { SearchStyles } from "../util/types";
+import type { EnsembleWidgetProps } from "../util/utils";
+import { getColor } from "../util/utils";
 
-type SelectOption = {
+interface SelectOption {
   label: Expression<string>;
   value: Expression<string | number>;
-};
+}
 
 export type MultiSelectProps = {
   data: Expression<SelectOption[]>;
@@ -20,18 +23,19 @@ export type MultiSelectProps = {
   placeholder?: Expression<string>;
 } & EnsembleWidgetProps<SearchStyles>;
 
-const MultiSelect: React.FC<MultiSelectProps> = ({
-  data,
-  labelKey,
-  valueKey,
-  default: defaultOptions,
-  placeholder,
-  styles,
-}) => {
+const MultiSelect: React.FC<MultiSelectProps> = (props) => {
+  const {
+    data,
+    labelKey,
+    valueKey,
+    default: defaultOptions,
+    placeholder,
+    styles,
+  } = props;
   const [options, setOptions] = useState<SelectOption[]>([]);
   const [newOption, setNewOption] = useState("");
   const [selectedValues, setSelectedValues] = useState<string[] | undefined>(
-    defaultOptions?.map((item) => item.value?.toString())
+    defaultOptions?.map((item) => item.value.toString()),
   );
 
   const templateData = useTemplateData(data);
@@ -39,8 +43,8 @@ const MultiSelect: React.FC<MultiSelectProps> = ({
   useEffect(() => {
     if (Array.isArray(templateData)) {
       const initialOptions = templateData.map((item) => ({
-        label: get(item, labelKey ?? "label"),
-        value: get(item, valueKey ?? "value"),
+        label: get(item, labelKey ?? "label") as Expression<string>,
+        value: get(item, valueKey ?? "value") as Expression<string | number>,
       }));
 
       if (defaultOptions) {
@@ -67,9 +71,41 @@ const MultiSelect: React.FC<MultiSelectProps> = ({
     }
   };
 
+  const { values } = useRegisterBindings(
+    { value: selectedValues, options },
+    props.id,
+    {
+      setSelectedValues,
+      setOptions,
+    },
+  );
+
   return (
     <SelectComponent
+      allowClear
+      defaultValue={defaultOptions?.map((item) => item.value.toString())}
+      // eslint-disable-next-line react/no-unstable-nested-components
+      dropdownRender={(menu) => <Dropdown menu={menu} newOption={newOption} />}
+      filterOption={(input, option) =>
+        option?.label
+          .toString()
+          .toLowerCase()
+          .startsWith(input.toLowerCase()) || false
+      }
       mode="tags"
+      notFoundContent={<></>}
+      onChange={handleChange}
+      onSearch={(v) => {
+        if (
+          values.options.some((option) =>
+            option.label.toString().toLowerCase().startsWith(v.toLowerCase()),
+          )
+        )
+          setNewOption("");
+        else setNewOption(v);
+      }}
+      options={values.options}
+      placeholder={placeholder ?? "Select"}
       style={{
         width: styles?.width ?? "100%",
         margin: styles?.margin,
@@ -80,56 +116,38 @@ const MultiSelect: React.FC<MultiSelectProps> = ({
           ? getColor(styles.borderColor)
           : undefined,
       }}
-      placeholder={placeholder ?? "Select"}
-      onChange={handleChange}
-      value={selectedValues}
-      defaultValue={defaultOptions?.map((item) => item.value?.toString())}
-      notFoundContent={<></>}
-      options={options}
-      allowClear
-      filterOption={(input, option) =>
-        option?.label
-          ?.toString()
-          .toLowerCase()
-          .startsWith(input.toLowerCase()) || false
-      }
-      onSearch={(v) => {
-        if (
-          options.some(
-            (option) =>
-              option?.label
-                ?.toString()
-                .toLowerCase()
-                .startsWith(v.toLowerCase())
-          )
-        )
-          setNewOption("");
-        else setNewOption(v);
-      }}
-      dropdownRender={(menu) => (
-        <>
-          {menu}
-          {newOption && (
-            <Space
-              style={{
-                padding: "10px 15px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                width: "100%",
-                cursor: "pointer",
-              }}
-            >
-              <span>There are no matches</span>
-              <Space>
-                <PlusCircleOutlined />
-                <span>{`Add "${newOption}"`}</span>
-              </Space>
-            </Space>
-          )}
-        </>
-      )}
+      value={values.value}
     />
+  );
+};
+
+interface DropdownProps {
+  menu: ReactElement;
+  newOption: string;
+}
+const Dropdown: React.FC<DropdownProps> = ({ menu, newOption }) => {
+  return (
+    <>
+      {menu}
+      {newOption ? (
+        <Space
+          style={{
+            padding: "10px 15px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            width: "100%",
+            cursor: "pointer",
+          }}
+        >
+          <span>There are no matches</span>
+          <Space>
+            <PlusCircleOutlined />
+            <span>{`Add "${newOption}"`}</span>
+          </Space>
+        </Space>
+      ) : null}
+    </>
   );
 };
 
