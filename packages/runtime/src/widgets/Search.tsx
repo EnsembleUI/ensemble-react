afd0d6780830943a42c839e8c5a067316a0d61fc
anserwaseem <hafiz.anser.waseem@gmail.com>
2023-10-09 21:26:26 +0500

    Add onValueChange and onSelect in Search widget

--- packages/runtime/src/widgets/Search.tsx
+++ packages/runtime/src/widgets/Search.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { useTemplateData, type Expression } from "framework";
+import {
+  useTemplateData,
+  type Expression,
+  type EnsembleAction,
+  useRegisterBindings,
+} from "framework";
 import type { SelectProps } from "antd";
 import { AutoComplete, Input } from "antd";
 import { SearchOutlined } from "@mui/icons-material";
@@ -7,6 +12,7 @@ import { get, isObject } from "lodash-es";
 import { WidgetRegistry } from "../registry";
 import type { SearchStyles } from "../util/types";
 import { getColor } from "../util/utils";
+import { useEnsembleAction } from "../runtime/hooks/useEnsembleAction";
 
 interface EnsembleWidgetProps<T> {
   id?: string;
@@ -18,23 +24,38 @@ export type SearchProps = {
   placeholder?: string;
   data?: Expression<object>;
   searchKey?: string;
+  onValueChange?: EnsembleAction;
+  onSelect?: EnsembleAction;
 } & EnsembleWidgetProps<SearchStyles>;
 
 export const Search: React.FC<SearchProps> = ({
+  id,
   placeholder,
   data,
   searchKey,
+  onValueChange,
+  onSelect,
   styles,
 }) => {
   const [options, setOptions] = useState<SelectProps<object>["options"]>([]);
+  const [value, setValue] = useState("");
+  // eslint-disable-next-line no-empty-pattern
+  const {} = useRegisterBindings(
+    {
+      value,
+    },
+    id,
+    { setValue },
+  );
 
-  const templateData = useTemplateData(data!);
+  const templateData = useTemplateData(data ?? "");
+  const valueChangeAction = useEnsembleAction(onValueChange);
+  const selectAction = useEnsembleAction(onSelect);
 
-  // TODO: Pass in search predicate function via props or filter via API
-  const handleSearch = (value: string) => {
+  const handleSearch = (search: string): void => {
     if (Array.isArray(templateData)) {
       setOptions(
-        value
+        search
           ? templateData
               .filter(
                 (item) =>
@@ -42,7 +63,7 @@ export const Search: React.FC<SearchProps> = ({
                   (isObject(item) ? get(item, searchKey ?? "") : item)
                     ?.toString()
                     ?.toLowerCase()
-                    ?.includes(value.toLowerCase()),
+                    ?.includes(search.toLowerCase()),
               )
               .map((item) => ({
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -52,6 +73,25 @@ export const Search: React.FC<SearchProps> = ({
               }))
           : [],
       );
+    } else if (onValueChange) {
+      (async (): Promise<void> => {
+        const apiResponse = await valueChangeAction?.callback();
+
+        if (apiResponse) {
+          setOptions(
+            findValuesByKey<string>(apiResponse, searchKey ?? "")
+              .filter((item) =>
+                item.toLowerCase().includes(search.toLowerCase()),
+              )
+              .map((item) => ({
+                value: item,
+                label: item,
+              })),
+          );
+        }
+      })().catch((e) => {
+        throw e;
+      });
     }
   };
 
@@ -59,9 +99,10 @@ export const Search: React.FC<SearchProps> = ({
     <AutoComplete
       allowClear
       onSearch={handleSearch}
-      // TODO: Handle on search result select
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      onSelect={() => {}}
+      onSelect={(selectedValue: string): void => {
+        setValue(selectedValue);
+        onSelect && selectAction?.callback();
+      }}
       options={options}
       popupMatchSelectWidth={styles?.width}
       size="large"
@@ -87,3 +128,19 @@ export const Search: React.FC<SearchProps> = ({
 };
 
 WidgetRegistry.register("Search", Search);
+
+function findValuesByKey<T>(obj: any, key: string): T[] {
+  let results: T[] = [];
+
+  if (obj && typeof obj === "object") {
+    for (const k in obj) {
+      if (k === key) results.push(obj[k]);
+      else {
+        const nestedResults = findValuesByKey<T>(obj[k], key);
+        results = results.concat(nestedResults);
+      }
+    }
+  }
+
+  return results;
+}
