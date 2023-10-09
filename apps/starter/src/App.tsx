d1ba240322f73c06b68a006d852c6e8e5bdf8221
sanaf1333 <sanaf1333@gmail.com>
2023-10-10 01:39:55 +0500

    updated collection card

--- apps/starter/src/App.tsx
+++ apps/starter/src/App.tsx
@@ -7,7 +7,7 @@ import DashboardYAML from "./ensemble/dashboard.yaml";
 import AskEmbraceYAML from "./ensemble/askEmbrace.yaml";
 import CreateGroupYAML from "./ensemble/createGroup.yaml";
 import InviteYAML from "./ensemble/invite.yaml";
-import ProductAndDevelopment from "./ensemble/productAndDevelopment.yaml";
+import ProductAndDevelopmentYAML from "./ensemble/productAndDevelopment.yaml";
 
 import "./App.css";
 
@@ -53,7 +53,7 @@ const testApp: ApplicationDTO = {
     {
       id: "productAndDevelopment",
       name: "Product and Development",
-      content: String(ProductAndDevelopment),
+      content: String(ProductAndDevelopmentYAML),
     },
   ],
 };
