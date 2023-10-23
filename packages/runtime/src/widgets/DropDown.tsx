033593603b94dafd67a1611f1d871ec90e66c34f
Syed Sibteali Baqar <sibteali786@gmail.com>
2023-10-23 21:36:05 +0500

    added and hooked dropwdown widget

---
+++ packages/runtime/src/widgets/DropDown.tsx
@@ -0,0 +1,48 @@
+import { Form as AntForm, Select } from "antd";
+import { useState } from "react";
+import { useRegisterBindings } from "framework";
+import type { Expression } from "framework";
+import { WidgetRegistry } from "../registry";
+import type { EnsembleWidgetProps } from "../util/types";
+
+export type DropdownProps = {
+  label?: string;
+  hintText?: string;
+  items: SelectOption[];
+} & EnsembleWidgetProps;
+
+interface SelectOption {
+  label: Expression<string>;
+  value: Expression<string | number>;
+}
+
+const Dropdown: React.FC<DropdownProps> = (props) => {
+  const [selectedValue, setSelectedValue] = useState<string>();
+  const { values } = useRegisterBindings(
+    { ...props, selectedValue },
+    props.id,
+    {
+      setSelectedValue,
+    }
+  );
+  const handleChange = (value: string) => {
+    setSelectedValue(value);
+  };
+  return (
+    <AntForm.Item label={values.label} name={values.id}>
+      <Select
+        onChange={handleChange}
+        placeholder={values?.hintText ? values.hintText : ""}
+        value={values.selectedValue}
+      >
+        {props.items.map((option, index) => (
+          <Select.Option key={index} value={option.value}>
+            {option.label}
+          </Select.Option>
+        ))}
+      </Select>
+    </AntForm.Item>
+  );
+};
+
+WidgetRegistry.register("Dropdown", Dropdown);
