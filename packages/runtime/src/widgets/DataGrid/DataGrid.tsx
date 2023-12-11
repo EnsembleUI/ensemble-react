1263c93c2a9e2ef07331623d680629f7e946b6ee
Syed Sibteali Baqar <sibteali786@gmail.com>
2023-12-11 21:36:16 +0500

    Added styling option for DataGrid column headers

--- packages/runtime/src/widgets/DataGrid/DataGrid.tsx
+++ packages/runtime/src/widgets/DataGrid/DataGrid.tsx
@@ -29,14 +29,25 @@ interface DataColumn {
   };
 }
 
-export interface GridProps extends EnsembleWidgetProps {
+export type GridProps = {
   DataColumns: DataColumn[];
+  styles?: {
+    headerStyle: {
+      backgroundColor: string;
+      fontSize: string;
+      fontFamily: string;
+      fontWeight: string;
+      textColor: string;
+      isDivider: boolean;
+      borderBottom: string;
+    };
+  };
   "item-template": {
     data: Expression<object>;
     name: string;
     template: DataGridRowTemplate;
   };
-}
+} & EnsembleWidgetProps;
 
 export interface DataGridRowTemplate {
   name: "DataRow";
@@ -49,67 +60,104 @@ export const DataGrid: React.FC<GridProps> = ({
   DataColumns,
   "item-template": itemTemplate,
   id,
+  styles,
 }) => {
   const { resolvedWidgetId, rootRef } = useWidgetId(id);
   const { namedData } = useTemplateData({ ...itemTemplate });
-
+  const headerStyle = styles?.headerStyle;
+  console.log(headerStyle);
   return (
-    <Table
-      dataSource={namedData}
-      key={resolvedWidgetId}
-      ref={rootRef}
-      style={{ width: "100%" }}
-    >
-      {DataColumns.map((col, index) => {
-        return (
-          <Table.Column
-            dataIndex={itemTemplate.name}
-            filters={col.filter?.values.map(({ label, value }) => ({
-              text: label,
-              value,
-            }))}
-            key={col.label}
-            onFilter={
-              col.filter?.onFilter
-                ? (value, record): boolean =>
-                    Boolean(
-                      evaluate(defaultScreenContext, col.filter?.onFilter, {
-                        value,
-                        record,
-                        [itemTemplate.name]: get(
+    <>
+      <Table
+        dataSource={namedData}
+        key={resolvedWidgetId}
+        ref={rootRef}
+        style={{ width: "100%" }}
+      >
+        {DataColumns.map((col, index) => {
+          return (
+            <Table.Column
+              dataIndex={itemTemplate.name}
+              filters={col.filter?.values.map(({ label, value }) => ({
+                text: label,
+                value,
+              }))}
+              key={col.label}
+              onFilter={
+                col.filter?.onFilter
+                  ? (value, record): boolean =>
+                      Boolean(
+                        evaluate(defaultScreenContext, col.filter?.onFilter, {
+                          value,
                           record,
-                          itemTemplate.name,
-                        ) as unknown,
-                      }),
-                    )
-                : undefined
-            }
-            render={(_: unknown, record: unknown): ReactElement => {
-              return (
-                <DataCell
-                  columnIndex={index}
-                  data={record}
-                  scopeName={itemTemplate.name}
-                  template={itemTemplate.template}
-                />
-              );
-            }}
-            sorter={
-              col.sort?.compareFn
-                ? (a, b): number =>
-                    Number(
-                      evaluate(defaultScreenContext, col.sort?.compareFn, {
-                        a,
-                        b,
-                      }),
-                    )
-                : undefined
-            }
-            title={col.label}
-          />
-        );
-      })}
-    </Table>
+                          [itemTemplate.name]: get(
+                            record,
+                            itemTemplate.name,
+                          ) as unknown,
+                        }),
+                      )
+                  : undefined
+              }
+              render={(_: unknown, record: unknown): ReactElement => {
+                return (
+                  <DataCell
+                    columnIndex={index}
+                    data={record}
+                    scopeName={itemTemplate.name}
+                    template={itemTemplate.template}
+                  />
+                );
+              }}
+              sorter={
+                col.sort?.compareFn
+                  ? (a, b): number =>
+                      Number(
+                        evaluate(defaultScreenContext, col.sort?.compareFn, {
+                          a,
+                          b,
+                        }),
+                      )
+                  : undefined
+              }
+              title={col.label}
+            />
+          );
+        })}
+      </Table>
+      <style>
+        {`
+			.ant-table-thead > tr > th {
+				${
+          headerStyle?.backgroundColor
+            ? `background-color : ${headerStyle.backgroundColor} !important;`
+            : ""
+        }
+		${
+      headerStyle?.fontFamily
+        ? `font-family : ${headerStyle.fontFamily} !important;`
+        : ""
+    }
+		${
+      headerStyle?.fontSize
+        ? `font-size : ${headerStyle.fontSize} !important;`
+        : ""
+    }
+		${
+      headerStyle?.fontWeight
+        ? `font-weight : ${headerStyle.fontWeight} !important;`
+        : ""
+    }
+		${headerStyle?.textColor ? `color : ${headerStyle.textColor} !important;` : ""}
+			}
+			.ant-table-thead > tr > th::before{
+				${headerStyle?.isDivider ? `position : unset !important;` : ""}
+			}
+			.ant-table-thead > tr > th {
+				${!headerStyle?.borderBottom ? "border-bottom: none !important" : ""}
+			}
+		`}
+      </style>
+    </>
   );
 };
 
