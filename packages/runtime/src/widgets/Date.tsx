d6c200c43766f6c2db8c3064d8863df8e0fbd3f4
Anser Waseem <hafiz.anser.waseem@gmail.com>
2023-12-11 18:20:28 +0500

    fixes

--- packages/runtime/src/widgets/Date.tsx
+++ packages/runtime/src/widgets/Date.tsx
@@ -358,4 +358,4 @@ const CustomActionBar: React.FC<PickersActionBarProps> = (props) => {
 // returns true if date is in mm/dd/yyyy format
 const isDateValid = (date?: string): boolean =>
   Boolean(date) &&
-  /^(?:0[1-9]|1[0-2])\/(?:0[1-9]|[12][0-9]|3[01])\/\d{4}$/.test(date);
+  /^(?:0[1-9]|1[0-2])\/(?:0[1-9]|[12][0-9]|3[01])\/\d{4}$/.test(date!);
