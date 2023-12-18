18c0027d4a686c7de4ebacd81a8cabbf457cfc95
Anser Waseem <hafiz.anser.waseem@gmail.com>
2023-12-18 22:56:53 +0500

    refactoring

--- packages/framework/src/data/index.ts
+++ packages/framework/src/data/index.ts
@@ -1,7 +1,6 @@
 import type * as fetcher from "./fetcher";
 
 export * from "./fetcher";
-export * from "./dateFormatter";
 
 export interface Response {
   body?: string | object;
--- packages/framework/src/index.ts
+++ packages/framework/src/index.ts
@@ -3,5 +3,6 @@ export * from "./loader";
 export * from "./parser";
 export * from "./hooks";
 export * from "./data";
+export * from "./date";
 export * from "./state";
 export { evaluate } from "./evaluate";
