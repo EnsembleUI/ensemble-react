80c0054742f27859734e720cb6cf81d946b026a9
Evan Shi <4132143+evshi@users.noreply.github.com>
2024-01-08 13:41:56 -0800

    Fix test

--- apps/starter/src/App.test.tsx
+++ apps/starter/src/App.test.tsx
@@ -1,9 +1,1 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Ensemble app id", () => {
-  render(<App />);
-  const defaultElement = screen.getByText("Something went wrong:");
-  expect(defaultElement).toBeInTheDocument();
-});
+test.todo("Renders your Ensemble app");
