67ab5d0e43322c7d37258e84f1d12861b84e441a
Yifeng Evan Shi <4132143+evshi@users.noreply.github.com>
2024-01-26 02:57:39 -0800

    Add onTap to Icon (#356)

--- packages/runtime/src/widgets/Icon.tsx
+++ packages/runtime/src/widgets/Icon.tsx
@@ -3,13 +3,16 @@ import { useRegisterBindings } from "@ensembleui/react-framework";
 import { WidgetRegistry } from "../registry";
 import type { IconProps } from "../shared/types";
 import { getColor, getIcon } from "../shared/styles";
+import { useEnsembleAction } from "../runtime/hooks/useEnsembleAction";
 
-export const Icon: React.FC<IconProps> = (props) => {
+export const Icon: React.FC<IconProps> = ({ onTap, ...props }) => {
   const [color, setColor] = useState(props.color);
   const [name, setName] = useState(props.name);
   const [backgroundColor, setBackgroundColor] = useState(
     props.styles?.backgroundColor,
   );
+
+  const onTapActionCallback = useEnsembleAction(onTap);
 
   const { values } = useRegisterBindings(
     { ...props, color, name, backgroundColor },
@@ -21,47 +24,47 @@ export const Icon: React.FC<IconProps> = (props) => {
     },
   );
   const IconComponent = getIcon(values?.name ? values.name : "");
-  if (IconComponent) {
-    return (
-      <IconComponent
-        className={values?.styles?.names}
-        sx={{
-          ...values?.styles,
-          color: values?.color && getColor(String(values?.color)),
-          fontSize: props.size,
-          backgroundColor: `${
-            values?.styles?.backgroundColor
-              ? values.styles.backgroundColor
-              : "transparent"
-          }`,
-          padding: `${
-            values?.styles?.padding ? `${values.styles.padding}px` : "0px"
-          }`,
-          margin: `${
-            values?.styles?.margin ? `${values.styles.margin}px` : "0px"
-          }`,
-          borderRadius: `${
-            values?.styles?.borderRadius
-              ? `${values.styles.borderRadius}px`
-              : "0px"
-          }`,
-          borderWidth: `${
-            values?.styles?.borderWidth
-              ? `${values.styles.borderWidth}px`
-              : "0px"
-          }`,
-          borderColor: values?.styles?.borderColor
-            ? getColor(String(values.styles.borderColor))
-            : undefined,
-          borderStyle: values?.styles?.borderWidth ? "solid" : undefined,
-          ...(values?.styles?.visible === false
-            ? { display: "none" }
-            : undefined),
-        }}
-      />
-    );
+  if (!IconComponent) {
+    return null;
   }
-  return null;
+  return (
+    <IconComponent
+      className={values?.styles?.names}
+      onClick={(): unknown => onTapActionCallback?.callback()}
+      sx={{
+        ...values?.styles,
+        color: values?.color && getColor(String(values.color)),
+        fontSize: props.size,
+        backgroundColor: `${
+          values?.styles?.backgroundColor
+            ? values.styles.backgroundColor
+            : "transparent"
+        }`,
+        padding: `${
+          values?.styles?.padding ? `${values.styles.padding}px` : "0px"
+        }`,
+        margin: `${
+          values?.styles?.margin ? `${values.styles.margin}px` : "0px"
+        }`,
+        borderRadius: `${
+          values?.styles?.borderRadius
+            ? `${values.styles.borderRadius}px`
+            : "0px"
+        }`,
+        borderWidth: `${
+          values?.styles?.borderWidth ? `${values.styles.borderWidth}px` : "0px"
+        }`,
+        borderColor: values?.styles?.borderColor
+          ? getColor(String(values.styles.borderColor))
+          : undefined,
+        borderStyle: values?.styles?.borderWidth ? "solid" : undefined,
+        ...(values?.styles?.visible === false
+          ? { display: "none" }
+          : undefined),
+        ...values?.styles,
+      }}
+    />
+  );
 };
 
 WidgetRegistry.register("Icon", Icon);
