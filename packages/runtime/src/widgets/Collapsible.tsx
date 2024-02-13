86f1897a8f7a25874bd254ce10b3e896343111f9
sagar davara <sagardspeed2@gmail.com>
2024-02-13 22:18:52 +0530

    fix: remove unnecessary attributes from the collapsible wrapper

--- packages/runtime/src/widgets/Collapsible.tsx
+++ packages/runtime/src/widgets/Collapsible.tsx
@@ -188,12 +188,8 @@ export const Collapsible: React.FC<CollapsibleProps> = (props) => {
         },
       }}
     >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        onKeyDown={(e) => e.stopPropagation()}
-        role="button"
-        tabIndex={0}
-      >
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+      <div onClick={(e) => e.stopPropagation()}>
         <Collapse
           accordion={values?.isAccordion}
           activeKey={activeValue}
