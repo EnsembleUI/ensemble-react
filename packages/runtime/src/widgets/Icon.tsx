aa275a62ca5dcc5ff6fa7611f8c1d89cb49dfec1
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-02-19 19:23:28 +0500

    FEATURE: added onMouseLeave action

--- packages/runtime/src/widgets/Icon.tsx
+++ packages/runtime/src/widgets/Icon.tsx
@@ -5,14 +5,21 @@ import type { IconProps } from "../shared/types";
 import { getColor, getIcon } from "../shared/styles";
 import { useEnsembleAction } from "../runtime/hooks/useEnsembleAction";
 
-export const Icon: React.FC<IconProps> = ({ onTap, onHover, ...props }) => {
+export const Icon: React.FC<IconProps> = ({
+  onTap,
+  onMouseEnter,
+  onMouseLeave,
+  ...props
+}) => {
   const [color, setColor] = useState(props.color);
   const [name, setName] = useState(props.name);
   const [backgroundColor, setBackgroundColor] = useState(
     props.styles?.backgroundColor,
   );
+  const [isMouseOver, setIsMouseOver] = useState(false);
   const onTapActionCallback = useEnsembleAction(onTap);
-  const onHoverActionCallback = useEnsembleAction(onHover);
+  const onMouseEnterAction = useEnsembleAction(onMouseEnter);
+  const onMouseLeaveAction = useEnsembleAction(onMouseLeave);
 
   const { values } = useRegisterBindings(
     { ...props, color, name, backgroundColor },
@@ -27,19 +34,31 @@ export const Icon: React.FC<IconProps> = ({ onTap, onHover, ...props }) => {
   if (!IconComponent) {
     return null;
   }
-  const handleMouseEnter = (
-    event: React.MouseEvent<SVGSVGElement, MouseEvent>,
-  ) => {
+  const handleMouseOver = (event: React.MouseEvent<SVGSVGElement>): void => {
     const { clientX, clientY } = event;
-    onHoverActionCallback?.callback({
-      location: { x: `${clientX}px`, y: `${clientY}px` },
-    });
+    if (!isMouseOver) {
+      setIsMouseOver(true);
+      onMouseEnterAction?.callback({
+        location: { x: clientX, y: clientY },
+      });
+    }
+  };
+  const handleMouseLeave = (event: React.MouseEvent<SVGSVGElement>): void => {
+    // Check if the mouse has left the Icon component
+    if (
+      !event.relatedTarget ||
+      !event.currentTarget.contains(event.relatedTarget as Node)
+    ) {
+      onMouseLeaveAction?.callback();
+      setIsMouseOver(false);
+    }
   };
   return (
     <IconComponent
       className={values?.styles?.names}
       onClick={(): unknown => onTapActionCallback?.callback()}
-      onMouseEnter={handleMouseEnter}
+      onMouseEnter={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
       sx={{
         ...values?.styles,
         color: values?.color && getColor(String(values.color)),
@@ -49,6 +68,7 @@ export const Icon: React.FC<IconProps> = ({ onTap, onHover, ...props }) => {
             ? values.styles.backgroundColor
             : "transparent"
         }`,
+        zIndex: "100000",
         padding: `${
           values?.styles?.padding ? `${values.styles.padding}px` : "0px"
         }`,
