f3d44590cec53e32071eb85b96bc250c08162fa0
Evan Shi <4132143+evshi@users.noreply.github.com>
2024-03-05 21:33:43 -0800

    Fix card styling schema

--- packages/runtime/src/runtime/runtime.tsx
+++ packages/runtime/src/runtime/runtime.tsx
@@ -1,10 +1,14 @@
 import type { EnsembleWidget } from "@ensembleui/react-framework";
 import type { ReactNode } from "react";
 import { isValidElement } from "react";
+import { isEmpty } from "lodash-es";
 import { WidgetRegistry } from "../registry";
 
 export const EnsembleRuntime = {
   render: (widgets: EnsembleWidget[]): ReactNode[] => {
+    if (isEmpty(widgets)) {
+      return [];
+    }
     return widgets.map((child, index) => {
       const result = WidgetRegistry.find(child.name);
       if (isValidElement(result)) {
