e91ba5fbd45139014cb2d02bd56a301bf47379e8
sagar davara <sagardspeed2@gmail.com>
2024-03-13 14:13:33 +0530

    fix: added new theme provider

---
+++ packages/runtime/src/runtime/theme.tsx
@@ -0,0 +1,36 @@
+import {
+  CustomThemeProvider,
+  useThemeContext,
+} from "@ensembleui/react-framework";
+import { ConfigProvider } from "antd";
+import { type PropsWithChildren } from "react";
+
+const DEFAULT_FONT_FAMILY = "sans-serif";
+
+export const EnsembleTheme: React.FC<PropsWithChildren> = ({ children }) => {
+  const themeScope = useThemeContext();
+
+  if (!themeScope.theme) {
+    return (
+      <CustomThemeProvider value={themeScope}>
+        <ConfigProvider>{children}</ConfigProvider>
+      </CustomThemeProvider>
+    );
+  }
+
+  return (
+    <CustomThemeProvider value={themeScope}>
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily:
+              themeScope.theme.Tokens?.Typography?.fontFamily ??
+              DEFAULT_FONT_FAMILY,
+          },
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </CustomThemeProvider>
+  );
+};
