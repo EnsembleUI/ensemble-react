87c64860fdec4b307534b8081a45a84146007e06
Evan Shi <4132143+evshi@users.noreply.github.com>
2024-03-14 10:13:45 -0700

    Refactor navigateUrl to use useEvaluate only

--- packages/runtime/src/runtime/hooks/useNavigateUrl.ts
+++ packages/runtime/src/runtime/hooks/useNavigateUrl.ts
@@ -2,15 +2,11 @@ import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   type NavigateUrlAction,
-  findExpressions,
   useCustomScope,
   useEnsembleStorage,
-  useScreenContext,
-  isExpression,
   useEvaluate,
 } from "@ensembleui/react-framework";
-import { cloneDeep, isString, set } from "lodash-es";
-import { evaluate } from "@ensembleui/react-framework/src/evaluate/evaluate";
+import { cloneDeep, isString } from "lodash-es";
 import type { EnsembleActionHook } from "./useEnsembleAction";
 
 export const useNavigateUrl: EnsembleActionHook<NavigateUrlAction> = (
@@ -19,54 +15,33 @@ export const useNavigateUrl: EnsembleActionHook<NavigateUrlAction> = (
   const navigate = useNavigate();
   const hasOptions = !isString(action);
   const screenUrl = hasOptions ? action?.url : action;
-  const screenContext = useScreenContext();
+  const inputs = useMemo(
+    () => (!isString(action) && action?.inputs ? cloneDeep(action.inputs) : {}),
+    [action],
+  );
   const customScope = useCustomScope();
   const storage = useEnsembleStorage();
-  const context = {
-    ensemble: {
-      storage,
-    },
-    ...customScope,
-  };
   const evaluatedRes = useEvaluate(
-    { url: screenUrl },
+    { url: screenUrl, inputs },
     {
-      context,
+      context: {
+        ensemble: {
+          storage,
+        },
+        ...customScope,
+      },
     },
   );
+
   const callback = useMemo(() => {
-    if (!screenUrl) {
+    if (!evaluatedRes.url) {
       return;
     }
 
-    const inputs =
-      !isString(action) && action?.inputs ? cloneDeep(action.inputs) : {};
-
-    if (screenContext) {
-      const expressionMap: string[][] = [];
-      findExpressions(inputs, [], expressionMap);
-      expressionMap.forEach(([path, value]) => {
-        const result = evaluate(screenContext, value, {
-          ensemble: {
-            storage,
-          },
-          ...customScope,
-        });
-        set(inputs, path, result);
-      });
-    }
-    if (isExpression(screenUrl)) {
-      return () => {
-        if (evaluatedRes.url) {
-          navigate(evaluatedRes.url, { state: inputs });
-        }
-      };
-    }
-
     return () => {
-      navigate(screenUrl, { state: inputs });
+      navigate(evaluatedRes.url, { state: evaluatedRes.inputs });
     };
-  }, [screenUrl, action, screenContext, storage, customScope, navigate]);
+  }, [evaluatedRes.inputs, evaluatedRes.url, navigate]);
 
   return callback ? { callback } : undefined;
 };
