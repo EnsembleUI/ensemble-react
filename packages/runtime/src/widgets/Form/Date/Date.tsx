b5820fe9d24366b2ee04bae9d2e2d5eefeccb986
sagar davara <sagardspeed2@gmail.com>
2024-04-02 23:12:10 +0530

    fix: type cast

--- packages/runtime/src/widgets/Form/Date/Date.tsx
+++ packages/runtime/src/widgets/Form/Date/Date.tsx
@@ -97,7 +97,7 @@ export const Date: React.FC<DateProps> = (props) => {
         onChange={onDateChange}
         placeholder={values?.hintText}
         style={{ width: "100%", ...values?.styles }}
-        value={dayjs(values?.value)}
+        value={dayjs(values?.value) as unknown as string}
         {...(values?.showCalendarIcon === false
           ? { suffixIcon: false }
           : undefined)}
