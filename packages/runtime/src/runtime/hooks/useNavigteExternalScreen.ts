3954ba611ce7d017b56ab7b773961dbb0cd079fd
sagar davara <sagardspeed2@gmail.com>
2024-04-11 18:13:03 +0530

    fix: pass invokeApi response to navigateExternalScreen

--- packages/runtime/src/runtime/hooks/useNavigteExternalScreen.ts
+++ packages/runtime/src/runtime/hooks/useNavigteExternalScreen.ts
@@ -1,25 +1,39 @@
 import { useMemo } from "react";
+import { isString, merge } from "lodash-es";
 import {
-  useEvaluate,
+  evaluate,
+  useEnsembleStorage,
+  useScreenContext,
   type NavigateExternalScreen,
 } from "@ensembleui/react-framework";
-import { isString } from "lodash-es";
 import { openExternalScreen } from "../navigation";
 import { type EnsembleActionHook } from "./useEnsembleAction";
 
 export const useNavigateExternalScreen: EnsembleActionHook<
   NavigateExternalScreen
 > = (action) => {
-  const evaluatedAction = useEvaluate(
-    isString(action) ? { url: action } : { ...action },
-  );
+  const screen = useScreenContext();
+  const storage = useEnsembleStorage();
+
   const callback = useMemo(() => {
-    if (!evaluatedAction || !evaluatedAction.url) {
+    if (!screen || !action) {
       return;
     }
 
-    return () => openExternalScreen(evaluatedAction as NavigateExternalScreen);
-  }, [evaluatedAction]);
+    return (args?: unknown) => {
+      const url = evaluate<{ [key: string]: unknown }>(
+        screen,
+        isString(action) ? action : action.url,
+        merge({ ensemble: { storage } }, args),
+      );
+
+      if (!url || !isString(url)) {
+        return;
+      }
+
+      return openExternalScreen({ url } as NavigateExternalScreen);
+    };
+  }, [screen, storage, action]);
 
   return callback ? { callback } : undefined;
 };
