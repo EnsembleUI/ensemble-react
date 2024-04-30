0e3500d50e188bfe8f0d286de6bca564d8ee7fc3
sagar davara <sagardspeed2@gmail.com>
2024-04-30 14:40:22 +0530

    fix: convert JS to style inject

--- packages/runtime/src/widgets/Stack.tsx
+++ packages/runtime/src/widgets/Stack.tsx
@@ -2,7 +2,6 @@ import {
   type EnsembleWidget,
   useRegisterBindings,
 } from "@ensembleui/react-framework";
-import { get, merge } from "lodash-es";
 import React, { useMemo } from "react";
 import { WidgetRegistry } from "../registry";
 import { EnsembleRuntime } from "../runtime";
@@ -29,124 +28,104 @@ type StackProps = {
 } & EnsembleWidgetProps<StackStyles>;
 
 // useful to calculate stack alighment styles based on alignment direction
-const calculateStackAlignment = (
-  alignment: string,
-): { [key: string]: unknown } => {
-  let alignmentStyles = {};
+const calculateStackAlignment = (id: string, alignment: string): string => {
+  let alignmentStyles = "";
 
   switch (alignment) {
     case "topLeft":
-      alignmentStyles = {
-        top: 0,
-        left: 0,
-      };
+      alignmentStyles = `
+        top: 0;
+        left: 0;
+      `;
       break;
 
     case "topCenter":
-      alignmentStyles = {
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-      };
+      alignmentStyles = `
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+      `;
       break;
 
     case "topRight":
-      alignmentStyles = {
-        top: 0,
-        right: 0,
-      };
+      alignmentStyles = `
+        top: 0;
+        right: 0;
+      `;
       break;
 
     case "centerLeft":
-      alignmentStyles = {
-        top: "50%",
-        left: 0,
-        transform: "translateY(-50%)",
-      };
+      alignmentStyles = `
+        top: 50%;
+        left: 0;
+        transform: translateY(-50%);
+      `;
       break;
 
     case "center":
-      alignmentStyles = {
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      };
+      alignmentStyles = `
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      `;
       break;
 
     case "centerRight":
-      alignmentStyles = {
-        top: "50%",
-        right: 0,
-        transform: "translateY(-50%)",
-      };
+      alignmentStyles = `
+        top: 50%;
+        right: 0;
+        transform: translateY(-50%);
+      `;
       break;
 
     case "bottomLeft":
-      alignmentStyles = {
-        bottom: 0,
-        left: 0,
-      };
+      alignmentStyles = `
+        bottom: 0;
+        left: 0;
+      `;
       break;
 
     case "bottomCenter":
-      alignmentStyles = {
-        bottom: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-      };
+      alignmentStyles = `
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+      `;
       break;
 
     case "bottomRight":
-      alignmentStyles = {
-        botton: 0,
-        right: 0,
-      };
+      alignmentStyles = `
+        botton: 0;
+        right: 0;
+      `;
       break;
 
     case "default":
-      alignmentStyles = {};
+      alignmentStyles = "";
       break;
   }
 
-  return { position: "absolute", ...alignmentStyles };
+  return `#${id} > :not(:first-child) {position: absolute; ${alignmentStyles}}`;
 };
 
 export const Stack: React.FC<StackProps> = (props) => {
   const { children, ...rest } = props;
-  const { values, rootRef } = useRegisterBindings({ ...rest }, props.id);
-
-  // add stack alignment css to childrens
-  const stackChildrens = useMemo(() => {
-    if (!children) return [];
-
-    return children.map((child, index) => {
-      const styles = get(child.properties, "styles") as {
-        [key: string]: unknown;
-      };
-
-      const stackStyles = index
-        ? calculateStackAlignment(values?.styles?.alignChildren || "center")
-        : {};
-
-      return {
-        ...child,
-        properties: {
-          ...child.properties,
-          styles: merge(styles, stackStyles),
-        },
-      };
-    });
-  }, [children, values]);
+  const { values, rootRef, id } = useRegisterBindings({ ...rest }, props.id);
 
   // render childrens
   const renderedChildren = useMemo(() => {
-    return EnsembleRuntime.render(stackChildrens);
-  }, [stackChildrens]);
+    return children ? EnsembleRuntime.render(children) : null;
+  }, [children]);
 
   return (
-    <div ref={rootRef} style={{ position: "relative" }}>
-      {renderedChildren}
-    </div>
+    <>
+      <div id={id} ref={rootRef} style={{ position: "relative" }}>
+        {renderedChildren}
+      </div>
+      <style>
+        {calculateStackAlignment(id, values?.styles?.alignChildren || "center")}
+      </style>
+    </>
   );
 };
 
