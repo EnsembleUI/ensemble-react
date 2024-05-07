23cf277f9e7ce51713c4060f455a882bf5d34514
Yifeng Evan Shi <4132143+evshi@users.noreply.github.com>
2024-05-07 14:27:47 -0700

    Support external links in sidebar menu items (#566)

--- packages/runtime/src/runtime/menu.tsx
+++ packages/runtime/src/runtime/menu.tsx
@@ -6,7 +6,7 @@ import {
   useRegisterBindings,
   type EnsembleWidget,
 } from "@ensembleui/react-framework";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getColor } from "../shared/styles";
 import { EnsembleRuntime } from "./runtime";
 
@@ -35,10 +35,12 @@ interface MenuItem {
   icon?: string;
   iconLibrary?: "default" | "fontAwesome";
   label: string;
-  page: string;
+  url?: string;
+  page?: string;
   selected?: boolean;
   divider?: boolean;
   hasNotifications?: boolean;
+  openNewTab?: boolean;
 }
 
 interface MenuBaseProps {
@@ -91,7 +93,6 @@ export const SideBarMenu: React.FC<MenuBaseProps> = ({ id, ...props }) => {
   const { values } = useRegisterBindings({ ...props, isCollapsed }, id, {
     setCollapsed,
   });
-  const navigate = useNavigate();
   const location = useLocation();
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
@@ -108,17 +109,13 @@ export const SideBarMenu: React.FC<MenuBaseProps> = ({ id, ...props }) => {
 
   useEffect(() => {
     const locationMatch = props.items.find(
-      (item) => `/${item.page.toLowerCase()}` === location.pathname,
+      (item) =>
+        item.page && `/${item.page.toLowerCase()}` === location.pathname,
     );
     if (locationMatch) {
       setSelectedItem(locationMatch.label);
     }
   }, [location.pathname, props.items]);
-
-  const handleClick = (page: string, label: string): void => {
-    setSelectedItem(label);
-    navigate(`/${page.toLowerCase()}`);
-  };
 
   return (
     <Col
@@ -162,7 +159,11 @@ export const SideBarMenu: React.FC<MenuBaseProps> = ({ id, ...props }) => {
                 props.styles?.iconHeight,
               )}
               key={item.page}
-              onClick={() => handleClick(item.page, item.label)}
+              onClick={(): void => {
+                if (!item.openNewTab) {
+                  setSelectedItem(item.label);
+                }
+              }}
               style={{
                 color:
                   selectedItem === item.label
@@ -193,7 +194,10 @@ export const SideBarMenu: React.FC<MenuBaseProps> = ({ id, ...props }) => {
                   : {}),
               }}
             >
-              <span>
+              <Link
+                target={item.openNewTab ? "_blank" : "_self"}
+                to={item.page ? `/${item.page}` : String(item.url)}
+              >
                 {!isCollapsed && item.label}
                 {item.hasNotifications ? (
                   <div
@@ -207,7 +211,7 @@ export const SideBarMenu: React.FC<MenuBaseProps> = ({ id, ...props }) => {
                     }}
                   />
                 ) : null}
-              </span>
+              </Link>
             </AntMenu.Item>
             {item.divider ? (
               <Col
