6d5b5dbba478e2998e2ea7a5661e299880145c0e
sagar davara <sagardspeed2@gmail.com>
2024-06-26 22:01:57 +0530

    fix: filename mistake

--- packages/framework/src/hooks/index.ts
+++ packages/framework/src/hooks/index.ts
@@ -11,4 +11,4 @@ export * from "./useHtmlPassThrough";
 export * from "./useEnsembleUser";
 export * from "./useThemeContext";
 export * from "./useEventContext";
-export * from "./useLaungaugeScope";
+export * from "./useLangaugeScope";
