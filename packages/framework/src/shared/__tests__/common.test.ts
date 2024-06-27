c475b7efca2b81aac4e928bc7631a97ef9e1105e
Sek Davara <sagardspeed2@gmail.com>
2024-06-27 10:51:33 +0530

    fix: added support for multiple expression evaluation (#668)
    Added support for multiple expression evaluation

--- packages/framework/src/shared/__tests__/common.test.ts
+++ packages/framework/src/shared/__tests__/common.test.ts
@@ -1,4 +1,4 @@
-import { findExpressions } from "../common";
+import { findExpressions, isCompoundExpression } from "../common";
 
 test("find deeply nested expressions", () => {
   /* eslint-disable no-template-curly-in-string */
@@ -7,6 +7,8 @@ test("find deeply nested expressions", () => {
       names: "test ${blah}",
       borderRadius: "${foo}",
       padding: [0, "${baz}", { value: "${beef}" }],
+      border: "${blah} xyz ${foo}",
+      borderColor: "${blah} ${foo}",
     },
     value: "${bar}",
   };
@@ -15,11 +17,61 @@ test("find deeply nested expressions", () => {
   findExpressions(testObj, [], expressionMap);
 
   expect(expressionMap).toEqual([
-    // ["styles.names", "test ${blah}"],
+    ["styles.names", "${`test ${blah}`}"],
     ["styles.borderRadius", "${foo}"],
     ["styles.padding.1", "${baz}"],
     ["styles.padding.2.value", "${beef}"],
+    ["styles.border", "${`${blah} xyz ${foo}`}"],
+    ["styles.borderColor", "${`${blah} ${foo}`}"],
     ["value", "${bar}"],
   ]);
   /* eslint-enable no-template-curly-in-string */
 });
+
+/* eslint-disable no-template-curly-in-string */
+describe("isCompoundExpression", () => {
+  test("Single placeholder with additional text", () => {
+    expect(isCompoundExpression("${abc} xys")).toBe(true);
+  });
+
+  test("Single placeholder with no additional text", () => {
+    expect(isCompoundExpression("${abc}")).toBe(false);
+  });
+
+  test("Multiple placeholders", () => {
+    expect(isCompoundExpression("${abc} ${def}")).toBe(true);
+  });
+
+  test("Single complex placeholder with expression", () => {
+    expect(isCompoundExpression("${abc + def}")).toBe(false);
+  });
+
+  test("Nested template literals (complex case)", () => {
+    expect(
+      isCompoundExpression(
+        '${`interpolate ${ensemble.storage.get("yyy")} multiple times ${ensemble.storage.get("yyy")}`}',
+      ),
+    ).toBe(false);
+  });
+
+  test("No placeholders", () => {
+    expect(isCompoundExpression("plain text")).toBe(false);
+  });
+
+  test("Placeholder at the start and end", () => {
+    expect(isCompoundExpression("${start} text ${end}")).toBe(true);
+  });
+
+  test("Placeholder within text", () => {
+    expect(isCompoundExpression("text ${placeholder} text")).toBe(true);
+  });
+
+  test("Nested placeholders", () => {
+    expect(isCompoundExpression("${a${b}c}")).toBe(false);
+  });
+
+  test("Multiple placeholders with nested placeholders", () => {
+    expect(isCompoundExpression("${a${b}c} ${d${e}f}")).toBe(true);
+  });
+});
+/* eslint-enable no-template-curly-in-string */
--- packages/framework/src/shared/common.ts
+++ packages/framework/src/shared/common.ts
@@ -4,10 +4,40 @@ export type Expression<T> = string | T;
 
 export const isExpression = (
   maybeExpression: Expression<unknown>,
-): maybeExpression is string =>
-  isString(maybeExpression) &&
-  maybeExpression.startsWith("${") &&
-  maybeExpression.endsWith("}");
+): maybeExpression is string => {
+  if (!isString(maybeExpression)) {
+    return false;
+  }
+
+  const regex = /\$\{[^}]+\}/g;
+  return regex.test(maybeExpression);
+};
+
+export const isCompoundExpression = (input: string): boolean => {
+  const placeholders = [];
+  let stack = 0,
+    start = -1;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "$" && input[i + 1] === "{") {
+      if (stack === 0) start = i;
+      stack++;
+      i++;
+    } else if (input[i] === "}") {
+      stack--;
+      if (stack === 0 && start !== -1) {
+        placeholders.push(input.slice(start, i + 1));
+        start = -1;
+      }
+    }
+  }
+
+  return (
+    placeholders.length > 1 ||
+    (placeholders.length === 1 &&
+      input.replace(placeholders[0], "").trim().length > 0)
+  );
+};
 
 export const sanitizeJs = (string: string): string => {
   const trimmedString = string.trim();
@@ -29,7 +59,10 @@ export const findExpressions = (
   Object.entries(obj).forEach(([key, value]) => {
     const curPath = path.concat(key);
     if (isExpression(value)) {
-      expressionMap.push([curPath.join("."), value]);
+      expressionMap.push([
+        curPath.join("."),
+        isCompoundExpression(value) ? `\${\`${value || ""}\`}` : value,
+      ]);
     } else if (isObject(value)) {
       findExpressions(value, curPath, expressionMap);
     }
