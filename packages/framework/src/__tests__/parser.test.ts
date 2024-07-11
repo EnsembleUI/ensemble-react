38d12b4be3d46f9d1ddcbfca205a87a22845cef8
sagar davara <sagardspeed2@gmail.com>
2024-07-11 15:37:52 +0530

    fix: add test case to check backward compatibility

--- packages/framework/src/__tests__/parser.test.ts
+++ packages/framework/src/__tests__/parser.test.ts
@@ -86,3 +86,66 @@ test("parses application with no custom widgets", () => {
     customWidgets: [],
   });
 });
+
+test("parses application with theme (old style)", () => {
+  const app = EnsembleParser.parseApplication({
+    screens: [
+      {
+        name: "home",
+        content: fs
+          .readFileSync(`${__dirname}/__resources__/helloworld.yaml`)
+          .toString(),
+      },
+    ],
+    theme: {
+      id: "theme",
+      content: fs
+        .readFileSync(`${__dirname}/__resources__/oldtheme.yaml`)
+        .toString(),
+    },
+    scripts: [],
+    name: "test",
+    id: "test",
+  } as unknown as ApplicationDTO);
+
+  expect(app).toMatchObject({
+    id: "test",
+    menu: undefined,
+    customWidgets: [],
+    themes: {
+      default: {},
+    },
+  });
+});
+
+test("parses application with theme (new style)", () => {
+  const app = EnsembleParser.parseApplication({
+    screens: [
+      {
+        name: "home",
+        content: fs
+          .readFileSync(`${__dirname}/__resources__/helloworld.yaml`)
+          .toString(),
+      },
+    ],
+    theme: {
+      id: "theme",
+      content: fs
+        .readFileSync(`${__dirname}/__resources__/newtheme.yaml`)
+        .toString(),
+    },
+    scripts: [],
+    name: "test",
+    id: "test",
+  } as unknown as ApplicationDTO);
+
+  expect(app).toMatchObject({
+    id: "test",
+    menu: undefined,
+    customWidgets: [],
+    themes: {
+      Light: {},
+      Dark: {},
+    },
+  });
+});
