e388d9c562f0a5e10e8dd9af0befbd5b03b34978
Anser Waseem <hafiz.anser.waseem@gmail.com>
2024-07-13 00:39:41 +0500

    compare invokable methods in register bindings too to get rid of stale methods

--- packages/framework/src/hooks/useRegisterBindings.ts
+++ packages/framework/src/hooks/useRegisterBindings.ts
@@ -1,6 +1,13 @@
 import type { RefCallback } from "react";
 import { useEffect, useMemo } from "react";
-import { get, isEmpty, isString, keys, debounce } from "lodash-es";
+import {
+  get,
+  isEmpty,
+  isString,
+  keys,
+  debounce,
+  isEqual as lodashIsEqual,
+} from "lodash-es";
 import isEqual from "react-fast-compare";
 import type { InvokableMethods } from "../state";
 import { useWidgetId } from "./useWidgetId";
@@ -58,7 +65,7 @@ export const useRegisterBindings = <T extends { [key: string]: unknown }>(
       (options?.comparator
         ? options.comparator(newValues, widgetState?.values)
         : isEqual(newValues, widgetState?.values)) &&
-      isEqual(keys(methods), keys(widgetState?.invokable.methods))
+      lodashIsEqual(keys(methods), keys(widgetState?.invokable.methods))
     ) {
       return;
     }
