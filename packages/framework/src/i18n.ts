036b96614dfd8a6b9f53700d2b2c06bb83562f48
Sek Davara <sagardspeed2@gmail.com>
2024-07-17 22:22:00 +0530

    fix: added translation support in studio (#701)
    Added translation support in studio

--- packages/framework/src/i18n.ts
+++ packages/framework/src/i18n.ts
@@ -2,6 +2,39 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { debug, error } from "./shared";
 
+export const languageMap: { [key: string]: string } = {
+  ar: "Arabic",
+  bn: "Bengali",
+  de: "German",
+  en: "English",
+  es: "Spanish",
+  fr: "French",
+  hi: "Hindi",
+  id: "Indonesian",
+  it: "Italian",
+  ja: "Japanese",
+  jv: "Javanese",
+  ko: "Korean",
+  ms: "Malay",
+  nl: "Dutch",
+  pa: "Punjabi",
+  pl: "Polish",
+  pt: "Portuguese",
+  ro: "Romanian",
+  ru: "Russian",
+  sv: "Swedish",
+  ta: "Tamil",
+  te: "Telugu",
+  th: "Thai",
+  tr: "Turkish",
+  uk: "Ukrainian",
+  ur: "Urdu",
+  vi: "Vietnamese",
+  zh: "Chinese",
+  el: "Greek",
+  da: "Danish",
+};
+
 i18n
   .use(initReactI18next)
   .init({
