18758f2d873bdf1e3a317316a664e27a43353c23
Evan Shi <4132143+evshi@users.noreply.github.com>
2024-08-13 13:58:27 -0700

    Create test cases for action group issues with data and storage

--- packages/runtime/src/runtime/hooks/__tests__/useActionGroup.test.tsx
+++ packages/runtime/src/runtime/hooks/__tests__/useActionGroup.test.tsx
@@ -1,6 +1,32 @@
-import { act, renderHook } from "@testing-library/react";
+/* eslint import/first: 0 */
+const fetchMock = jest.fn();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const frameworkActual = jest.requireActual("@ensembleui/react-framework");
+
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { useActionGroup } from "../useEnsembleAction";
+import { createCustomWidget } from "../../customWidget";
+import { Button } from "../../../widgets";
+import { EnsembleScreen } from "../../screen";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock("@ensembleui/react-framework", () => ({
+  ...frameworkActual,
+  DataFetcher: {
+    fetch: fetchMock,
+  },
+}));
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 test("execute multiple actions", () => {
   const logSpy = jest.spyOn(console, "log");
@@ -28,5 +54,137 @@ test("execute multiple actions", () => {
   expect(logSpy).toHaveBeenCalledWith("foo");
   expect(logSpy).toHaveBeenCalledWith("bar");
 });
-test.todo("fetch multiple APIs");
-test.todo("mutate multiple storage variables");
+
+test("fetch multiple APIs", () => {
+  const logSpy = jest.spyOn(console, "log");
+
+  fetchMock.mockResolvedValueOnce({
+    body: {
+      data: "foo",
+    },
+  });
+  fetchMock.mockResolvedValueOnce({
+    body: {
+      data: "bar",
+    },
+  });
+
+  render(
+    <EnsembleScreen
+      screen={{
+        name: "test",
+        id: "test",
+        body: {
+          name: "Row",
+          properties: {
+            children: [
+              {
+                name: "Button",
+                properties: {
+                  label: "test",
+                  onTap: {
+                    executeCode: {
+                      body: `
+                        console.log(test1?.body?.data)
+                        console.log(test2?.body?.data)
+                      `,
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+        apis: [
+          {
+            name: "test1",
+            method: "GET",
+          },
+          {
+            name: "test2",
+            method: "GET",
+          },
+        ],
+        onLoad: {
+          executeActionGroup: {
+            actions: [
+              {
+                invokeAPI: {
+                  name: "test1",
+                },
+              },
+              {
+                invokeAPI: {
+                  name: "test2",
+                },
+              },
+            ],
+          },
+        },
+      }}
+    />,
+    {
+      wrapper: BrowserRouter,
+    },
+  );
+
+  expect(fetchMock).toHaveBeenCalledTimes(2);
+
+  act(() => {
+    const button = screen.getByText("test");
+    fireEvent.click(button);
+  });
+
+  expect(logSpy).toHaveBeenCalledWith("foo");
+  expect(logSpy).toHaveBeenCalledWith("bar");
+});
+
+test("mutate multiple storage variables", () => {
+  const logSpy = jest.spyOn(console, "log");
+  const Test = createCustomWidget({
+    onLoad: {
+      executeActionGroup: {
+        actions: [
+          {
+            executeCode: "ensemble.storage.set('foo', 'bar')",
+          },
+          {
+            executeCode: "ensemble.storage.set('foo2', 'bar2')",
+          },
+        ],
+      },
+    },
+    name: "",
+    inputs: [],
+    body: {
+      name: "Row",
+      properties: {},
+    },
+  });
+
+  render(
+    <>
+      <Test events={{}} inputs={{}} />
+      <Button
+        label="test"
+        onTap={{
+          executeCode: {
+            body: `
+              console.log(ensemble.storage.get('foo'))
+              console.log(ensemble.storage.get('foo2'))
+            `,
+          },
+        }}
+      />
+    </>,
+    {
+      wrapper: BrowserRouter,
+    },
+  );
+
+  const button = screen.getByText("test");
+  fireEvent.click(button);
+
+  expect(logSpy).toHaveBeenCalledWith("bar");
+  expect(logSpy).toHaveBeenCalledWith("bar2");
+});
