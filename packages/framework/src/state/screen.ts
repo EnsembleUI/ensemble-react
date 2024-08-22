93b93e495e3257957535064e494bfcf04b67a580
Evan Shi <4132143+evshi@users.noreply.github.com>
2024-08-22 08:48:56 -0700

    Overwrite EnsembleResponse instead of merge

--- packages/framework/src/state/screen.ts
+++ packages/framework/src/state/screen.ts
@@ -1,7 +1,7 @@
 import { atom } from "jotai";
 import { focusAtom } from "jotai-optics";
-import { merge } from "lodash-es";
-import type { Response, WebSocketConnection } from "../data";
+import { isObject, mergeWith } from "lodash-es";
+import { type Response, type WebSocketConnection } from "../data";
 import type { EnsembleAppModel, EnsembleScreenModel } from "../shared";
 import type { WidgetState } from "./widget";
 
@@ -40,7 +40,11 @@ export const screenDataAtom = atom(
   (get) => get(screenDataFocusAtom),
   (get, set, update) => {
     const currentData = get(screenDataFocusAtom);
-    const nextData = merge({}, currentData, update);
+    const nextData = mergeWith({}, currentData, update, (objVal, srcVal) => {
+      if (isObject(objVal) && isObject(srcVal)) {
+        return srcVal;
+      }
+    });
     set(screenDataFocusAtom, nextData);
   },
 );
