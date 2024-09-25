b1447432acde2cb5531b8b278b502e9ee20d39a4
sagar davara <sagardspeed2@gmail.com>
2024-09-25 12:56:20 +0530

    fix: useFont hook optimization

--- packages/framework/src/hooks/useFonts.ts
+++ packages/framework/src/hooks/useFonts.ts
@@ -2,16 +2,14 @@ import { useEffect } from "react";
 import { error, type EnsembleFontModel } from "../shared";
 
 export const useFonts = (fonts?: EnsembleFontModel[]): void => {
-  const loadFontFace = (font: EnsembleFontModel): unknown => {
-    return new FontFace(font.family, `url(${font.url})`, font.options)
-      .load()
-      .then((loadedFont) => document.fonts.add(loadedFont))
-      .catch((e) => error(e));
-  };
-
   useEffect(() => {
     if (fonts) {
-      fonts.forEach(loadFontFace);
+      fonts.forEach((font: EnsembleFontModel) => {
+        new FontFace(font.family, `url(${font.url})`, font.options)
+          .load()
+          .then((loadedFont) => document.fonts.add(loadedFont))
+          .catch((e) => error(e));
+      });
     }
   }, [fonts]);
 };
