94ca82a51c0bf512b835278c368f65dde4593442
Sek Davara <sagardspeed2@gmail.com>
2024-10-04 12:27:28 +0530

    feat: modernaization radio widget (#824)
    radio mondernatization

--- packages/runtime/src/widgets/Form/Radio.tsx
+++ packages/runtime/src/widgets/Form/Radio.tsx
@@ -1,17 +1,44 @@
 import type { EnsembleAction, Expression } from "@ensembleui/react-framework";
-import { useRegisterBindings } from "@ensembleui/react-framework";
+import {
+  CustomScopeProvider,
+  useEvaluate,
+  useRegisterBindings,
+  useTemplateData,
+} from "@ensembleui/react-framework";
 import { Radio, Form } from "antd";
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { get, isEmpty, isObject, map } from "lodash-es";
 import { WidgetRegistry } from "../../registry";
 import type { EnsembleWidgetStyles, HasItemTemplate } from "../../shared/types";
 import { useEnsembleAction } from "../../runtime/hooks/useEnsembleAction";
+import { EnsembleRuntime } from "../../runtime";
 import type { FormInputProps } from "./types";
 import { EnsembleFormItem } from "./FormItem";
 
 const widgetName = "Radio";
 
+const withValueTemplate = <P extends object>(
+  WrappedComponent: React.ComponentType<P>,
+) =>
+  function Component(
+    props: P & {
+      templateValue?: Expression<string>;
+    },
+  ): React.ReactElement {
+    const { templateValue } = props;
+    const { evaluatedValue } = useEvaluate({
+      evaluatedValue: templateValue,
+    });
+
+    if (!templateValue) {
+      return <WrappedComponent {...props} />;
+    }
+
+    return <WrappedComponent {...props} value={evaluatedValue} />;
+  };
+
 export type RadioWidgetProps = FormInputProps<string> &
-  HasItemTemplate & {
+  HasItemTemplate & { "item-template"?: { value: Expression<string> } } & {
     items: {
       label: Expression<string>;
       value: Expression<string | number>;
@@ -21,28 +48,32 @@ export type RadioWidgetProps = FormInputProps<string> &
     onChange?: EnsembleAction;
   };
 
+interface RadioOptionsProps {
+  disabled: boolean;
+  value?: string | number;
+  children: React.ReactElement | string;
+  style: EnsembleWidgetStyles;
+  scope: { [key: string]: unknown };
+}
+
+const RadioComponent = withValueTemplate(Radio);
+
 export const RadioWidget: React.FC<RadioWidgetProps> = (props) => {
+  const { "item-template": itemTemplate, onChange, ...rest } = props;
   const [value, setValue] = useState<string | number | undefined>(undefined);
   const { values, rootRef } = useRegisterBindings(
-    { ...props, initialValue: props.value, value, widgetName },
-    props.id,
+    { ...rest, initialValue: rest.value, value, widgetName },
+    rest.id,
     { setValue },
   );
-  const action = useEnsembleAction(props.onChange);
 
-  const handleChange = useCallback(
-    (newValue: string) => {
-      setValue(newValue);
-      action?.callback({ value: newValue });
-    },
-    [action],
-  );
-
-  const formInstance = Form.useFormInstance();
-
+  // update initial value
   useEffect(() => {
-    setValue(values?.initialValue?.toString());
+    setValue(values?.initialValue);
   }, [values?.initialValue]);
+
+  // update form field initial value
+  const formInstance = Form.useFormInstance();
 
   useEffect(() => {
     if (formInstance) {
@@ -52,6 +83,68 @@ export const RadioWidget: React.FC<RadioWidgetProps> = (props) => {
     }
   }, [value, formInstance]);
 
+  // onchange ensemble action
+  const onChangeAction = useEnsembleAction(onChange);
+
+  // handle onchange of radio
+  const handleChange = useCallback(
+    (newValue: string) => {
+      setValue(newValue);
+      onChangeAction?.callback({ value: newValue });
+    },
+    [onChangeAction],
+  );
+
+  // extract template data
+  const { namedData } = useTemplateData({
+    data: itemTemplate?.data,
+    name: itemTemplate?.name,
+  });
+
+  // handle radio items
+  const radioItems = useMemo(() => {
+    const radioOptions: RadioOptionsProps[] = [];
+
+    const getStyle = (styles: EnsembleWidgetStyles): object => ({
+      ...styles,
+      ...(styles?.visible === false ? { display: "none" } : undefined),
+    });
+
+    if (values?.items) {
+      values.items.forEach((scope) => {
+        radioOptions.push({
+          scope,
+          children: scope.label,
+          disabled: values.enabled === false || scope.enabled === false,
+          value: scope.value,
+          style: getStyle(scope.styles as object),
+        });
+      });
+    }
+
+    if (isObject(itemTemplate) && !isEmpty(namedData)) {
+      map(namedData, (scope: { [key: string]: unknown }) => {
+        const typedItem = get(scope, itemTemplate.name) as {
+          [key: string]: unknown;
+        };
+        radioOptions.push({
+          scope,
+          disabled: values?.enabled === false,
+          children: <>{EnsembleRuntime.render([itemTemplate.template])}</>,
+          style: getStyle(typedItem.styles as object),
+        });
+      });
+    }
+
+    return radioOptions.map((option: RadioOptionsProps) => {
+      return (
+        <CustomScopeProvider key={option.value} value={option.scope}>
+          <RadioComponent {...option} templateValue={itemTemplate?.value} />
+        </CustomScopeProvider>
+      );
+    });
+  }, [values?.items, values?.enabled, itemTemplate, namedData]);
+
   return (
     <EnsembleFormItem values={values}>
       <Radio.Group
@@ -60,21 +153,7 @@ export const RadioWidget: React.FC<RadioWidgetProps> = (props) => {
         style={values?.styles}
         value={String(values?.value)}
       >
-        {values?.items.map((item) => (
-          <Radio
-            disabled={values.enabled === false || item.enabled === false}
-            key={item.value}
-            style={{
-              ...item.styles,
-              ...(item.styles?.visible === false
-                ? { display: "none" }
-                : undefined),
-            }}
-            value={String(item.value)}
-          >
-            {item.label}
-          </Radio>
-        ))}
+        {radioItems}
       </Radio.Group>
     </EnsembleFormItem>
   );
