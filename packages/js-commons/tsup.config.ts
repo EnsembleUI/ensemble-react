59be8543a34037de3851e1862d3616f0aac60b14
Yifeng Evan Shi <4132143+evshi@users.noreply.github.com>
2024-11-05 15:36:40 -0800

    Bootstrap js-commons package (#893)

---
+++ packages/js-commons/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs"],
+  external: ["react"],
+  bundle: true,
+  splitting: false,
+  dts: true,
+});
