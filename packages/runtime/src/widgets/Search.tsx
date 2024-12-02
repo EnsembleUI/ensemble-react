4b2c4fd8fe302e8c2975dd97c79ae622de97ea49
Sharjeel Yunus <61178058+sharjeelyunus@users.noreply.github.com>
2024-12-02 21:18:15 +0500

    search widget with preload results (#927)

--- packages/runtime/src/widgets/Search.tsx
+++ packages/runtime/src/widgets/Search.tsx
@@ -38,6 +38,7 @@ export type SearchProps = {
   onClear?: EnsembleAction;
   iconStyles?: EnsembleWidgetStyles;
   notFoundContent?: Expression<string> | { [key: string]: unknown };
+  searchIfEmpty: boolean;
 } & EnsembleWidgetProps &
   HasItemTemplate & {
     "item-template"?: { value: Expression<string> };
@@ -52,6 +53,7 @@ export const Search: React.FC<SearchProps> = ({
   onSelect,
   onClear,
   value: initialValue,
+  searchIfEmpty,
   ...rest
 }) => {
   const [searchValue, setSearchValue] = useState<string | null>(null);
@@ -88,7 +90,7 @@ export const Search: React.FC<SearchProps> = ({
   );
 
   const renderOptions = useMemo(() => {
-    if (isEmpty(searchValue)) return [];
+    if (isEmpty(searchValue) && !searchIfEmpty) return [];
 
     let dropdownOptions: JSX.Element[] = [];
 
