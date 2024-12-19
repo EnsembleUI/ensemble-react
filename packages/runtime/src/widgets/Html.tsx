78b68088d47dd4b0f76c10902444eaffa0b9618f
sagar davara <sagardspeed2@gmail.com>
2024-12-19 22:47:33 +0530

    feat: added html widget

---
+++ packages/runtime/src/widgets/Html.tsx
@@ -0,0 +1,57 @@
+import { useMemo } from "react";
+import {
+  useRegisterBindings,
+  type Expression,
+} from "@ensembleui/react-framework";
+import { WidgetRegistry } from "../registry";
+import type { EnsembleWidgetProps } from "../shared/types";
+
+const widgetName = "Html";
+
+interface HtmlCssSelectorStyle {
+  selector: Expression<string>;
+  properties: React.CSSProperties;
+}
+
+export type HtmlProps = {
+  text: Expression<string>;
+  cssStyles?: HtmlCssSelectorStyle[];
+} & EnsembleWidgetProps;
+
+export const Html: React.FC<HtmlProps> = (props) => {
+  const { values, rootRef } = useRegisterBindings(
+    { ...props, widgetName },
+    props.id,
+    {},
+  );
+
+  const cssStyles = useMemo(() => {
+    return (values?.cssStyles || []).reduce((styles, item) => {
+      const properties = Object.entries(item.properties)
+        .map(([key, value]) => {
+          // Convert camelCase to kebab-case for CSS properties
+          const cssKey = key.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`);
+          return `${cssKey}: ${String(value)}`;
+        })
+        .join(";");
+
+      return `${styles}
+        ${item.selector} {
+          ${properties}
+        }
+      `;
+    }, "");
+  }, [values?.cssStyles]);
+
+  return (
+    <>
+      <style>{cssStyles}</style>
+      <div
+        dangerouslySetInnerHTML={{ __html: values?.text || "" }}
+        ref={rootRef}
+      />
+    </>
+  );
+};
+
+WidgetRegistry.register(widgetName, Html);
